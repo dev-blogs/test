94049e4a56c927e0959a84fbeb9667d3bb208729
Evgenii <zheka0783@yandex.ru>
2021-10-30 15:21:33 +0300

    Changed to spring-boot. Modified pom.xml

---
+++ src/main/java/org/example/TestController.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return "test";
+    }
+}
