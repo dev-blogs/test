b92a47b071a85500965602ff285b1fdde38b81e7
zheka <zheka0783@yandex.ru>
2024-11-11 19:58:26 +0200

    Code development. Added output ip address of host where java app is being running

--- src/main/java/org/example/TestController.java
+++ src/main/java/org/example/TestController.java
@@ -2,6 +2,10 @@ package org.example;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
@@ -9,6 +13,21 @@ public class TestController {
 
     @GetMapping("/test")
     public String greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return "test message";
+        Enumeration e = null;
+        try {
+            e = NetworkInterface.getNetworkInterfaces();
+        } catch (SocketException ex) {
+            throw new RuntimeException(ex);
+        }
+        StringBuilder builder = new StringBuilder();
+        while(e.hasMoreElements()) {
+            NetworkInterface n = (NetworkInterface) e.nextElement();
+            Enumeration ee = n.getInetAddresses();
+            while (ee.hasMoreElements()) {
+                InetAddress i = (InetAddress) ee.nextElement();
+                builder.append(i.getHostAddress() + " ");
+            }
+        }
+        return "Test message " + builder;
     }
 }
