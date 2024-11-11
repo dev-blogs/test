1aa6b0c935be32d52af4c0bf591cf4614c5c1463
zheka <zheka0783@yandex.ru>
2024-11-11 20:07:56 +0200

    Code refactoring

--- src/main/java/org/example/TestController.java
+++ src/main/java/org/example/TestController.java
@@ -7,27 +7,28 @@ import java.net.NetworkInterface;
 import java.net.SocketException;
 import java.util.Enumeration;
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.Stream;
 
 @RestController
 public class TestController {
 
     @GetMapping("/test")
     public String greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-        Enumeration e = null;
+        Enumeration networkInterfaces = null;
         try {
-            e = NetworkInterface.getNetworkInterfaces();
+            networkInterfaces = NetworkInterface.getNetworkInterfaces();
         } catch (SocketException ex) {
             throw new RuntimeException(ex);
         }
         StringBuilder builder = new StringBuilder();
-        while(e.hasMoreElements()) {
-            NetworkInterface n = (NetworkInterface) e.nextElement();
-            Enumeration ee = n.getInetAddresses();
-            while (ee.hasMoreElements()) {
-                InetAddress i = (InetAddress) ee.nextElement();
-                builder.append(i.getHostAddress() + " ");
+        while(networkInterfaces.hasMoreElements()) {
+            NetworkInterface networkInterface = (NetworkInterface) networkInterfaces.nextElement();
+            Enumeration inetAddresses = networkInterface.getInetAddresses();
+            while (inetAddresses.hasMoreElements()) {
+                InetAddress inetAddress = (InetAddress) inetAddresses.nextElement();
+                builder.append(inetAddress.getHostAddress() + "</br>");
             }
         }
-        return "Test message " + builder;
+        return "Test message. IP addresses:</br> " + builder;
     }
 }
